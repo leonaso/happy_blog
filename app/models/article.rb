0b6a780e702b54363c9a544bf9c9ab344565553f
leonaso <leoar_soto@yahoo.com>
2015-12-08 00:11:04 +0000

    Added Article model

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+  
+  validates :article, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
+  
+end
